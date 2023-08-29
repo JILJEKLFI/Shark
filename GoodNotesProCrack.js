88832dc3fe78908ab39e4896b02d110c46e1daec
huozongsheng <huozongsheng@outlook.com>
2023-08-29 16:21:12 +0800

    gn crack

--- GoodNotesProCrack.js
+++ GoodNotesProCrack.js
@@ -13,39 +13,65 @@
  hostname=isi.csan.goodnotes.com
 
  ***********************************/
-var gn6res = JSON.parse($response.body);
-gn6res.subscriber.non_subscriptions = {
-    "com.goodnotes6.one_time_unlock": [
-        {
-            "is_sandbox": false,
-            "ownership_type": "PURCHASED",
-            "id": "46ab585dbc",
-            "original_purchase_date": "2022-11-10T23:58:09Z",
-            "store_transaction_id": "300001271068792",
-            "purchase_date": "2023-08-10T23:58:09Z",
-            "store": "app_store",
-            "Warning" : "破解"
+
+$done({
+    body: JSON.stringify({
+        "request_date_ms": 1693285507211,
+        "request_date": "2023-08-29T05:05:07Z",
+        "subscriber": {
+            "non_subscriptions": {
+                "com.goodnotes6.one_time_unlock": [
+                    {
+                        "is_sandbox": false,
+                        "ownership_type": "PURCHASED",
+                        "id": "c3f10caf05",
+                        "original_purchase_date": "2023-08-24T03:45:41Z",
+                        "store_transaction_id": "350001668680087",
+                        "purchase_date": "2023-08-24T03:45:41Z",
+                        "store": "app_store"
+                    }
+                ]
+            },
+            "first_seen": "2023-08-25T11:25:05Z",
+            "original_application_version": "1442266.462619634",
+            "other_purchases": {
+                "com.goodnotes6.one_time_unlock": {
+                    "purchase_date": "2023-08-24T03:45:41Z"
+                }
+            },
+            "management_url": null,
+            "subscriptions": {},
+            "entitlements": {
+                "apple_access": {
+                    "grace_period_expires_date": null,
+                    "purchase_date": "2023-08-24T03:45:41Z",
+                    "product_identifier": "com.goodnotes6.one_time_unlock",
+                    "expires_date": null
+                },
+                "gn5": {
+                    "grace_period_expires_date": null,
+                    "purchase_date": "2022-01-04T13:41:52Z",
+                    "product_identifier": "rc_promo_gn5_lifetime",
+                    "expires_date": "2221-11-17T13:41:52Z"
+                },
+                "crossplatform_access": {
+                    "grace_period_expires_date": null,
+                    "purchase_date": "2023-08-24T03:45:41Z",
+                    "product_identifier": "com.goodnotes6.one_time_unlock",
+                    "expires_date": null,
+                    "Warning": "解锁"
+                },
+                "premium": {
+                    "grace_period_expires_date": null,
+                    "purchase_date": "2023-08-24T03:45:41Z",
+                    "product_identifier": "com.goodnotes6.one_time_unlock",
+                    "expires_date": null,
+                    "Warning": "解锁"
+                }
+            },
+            "original_purchase_date": "2022-01-04T13:41:52Z",
+            "original_app_user_id": "4438a86c-7033-455c-ab39-65837f9aecbf",
+            "last_seen": "2023-08-29T04:56:37Z"
         }
-    ]
-}
-gn6res.subscriber.other_purchases = {
-    "com.goodnotes6.one_time_unlock": {
-        "purchase_date": "2023-08-10T23:58:09Z",
-        "Warning": "破解"
-    }
-}
-gn6res.subscriber.entitlements.premium = {
-    "grace_period_expires_date": null,
-    "purchase_date": "2023-08-24T03:45:41Z",
-    "product_identifier": "com.goodnotes6.one_time_unlock",
-    "expires_date": null,
-    "Warning": "破解"
-}
-gn6res.subscriber.entitlements.apple_access = {
-    "grace_period_expires_date": null,
-    "purchase_date": "2023-08-10T14:16:41Z",
-    "product_identifier": "com.goodnotes6.one_time_unlock",
-    "expires_date": null,
-    "Warning": "破解"
-}
-$done({body: JSON.stringify(gn6res)});
+    })
+});
