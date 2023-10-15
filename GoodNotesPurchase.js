194bc95cbadd07737d8540e0a9c7d8e66069d1cd
huozongsheng <huozongsheng@outlook.com>
2023-10-15 22:05:31 +0800

    gn crack

--- GoodNotesPurchase.js
+++ GoodNotesPurchase.js
@@ -1,1 +1,6 @@
-$done({status:"HTTP/1.1 201 OK", body:"here is a string"});
+$done({
+    status: "HTTP/1.1 400 Bad Request", body: {
+        "message": "There is already another active subscriber using the same receipt.",
+        "code": 7102
+    }
+});
