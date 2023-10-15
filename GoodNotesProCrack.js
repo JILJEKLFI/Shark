1eee5a82d2762174ee7bc027548664c3abc747cf
huozongsheng <huozongsheng@outlook.com>
2023-10-15 11:26:22 +0800

    gn crack

--- GoodNotesProCrack.js
+++ GoodNotesProCrack.js
@@ -17,64 +17,65 @@
 $done({
     body: JSON.stringify({
         "active" : true,
-        "issued_at" : "2023-08-29T09:01:35.028865Z",
-        "id" : "4142b2e3-9b99-402c-8979-e9d11aca8c6f",
-        "expires_at" : "2221-11-17T21:41:52.690364Z",
+        "issued_at" : "2023-10-15T03:04:02.010859Z",
+        "id" : "ac60b11e-3156-43b6-8f78-2d1704095160",
+        "expires_at" : "2023-11-14T03:04:02.010859Z",
         "authenticator_assurance_level" : "aal1",
         "identity" : {
             "traits" : {
-                "email" : "b4k8znnv9t@privaterelay.appleid.com",
+                "email" : "note2622@outlook.com",
                 "last_name" : "",
-                "identity_provider" : "APPLE",
-                "accepted_tos" : 1692964698680,
-                "first_name" : "b4k8znnv9t",
-                "guid" : "4438a86c-7033-455c-ab39-65837f9aecbf"
+                "identity_provider" : "MICROSOFT",
+                "accepted_tos" : 1696830434292,
+                "first_name" : "G G",
+                "guid" : "5a1cf8b3-5d48-4418-826b-0530e55f47a4"
             },
             "schema_id" : "v1",
-            "id" : "f77fc589-281a-40cd-951f-cb23cec46d83",
-            "state_changed_at" : "2023-08-25T11:25:02.320976Z",
+            "id" : "7e818cf7-1413-4117-98b7-a7a5c581d6c6",
+            "state_changed_at" : "2023-10-09T05:28:31.856672Z",
             "metadata_public" : {
+                "is_email_fake" : false,
                 "registration_client_platform" : "APPLE",
+                "country" : "USA",
                 "registration_client_os" : "IOS",
-                "accepted_tos" : 1692964698680,
+                "accepted_tos" : 1696830434292,
                 "is_personal_data_anonymized" : false,
                 "entitlements" : {
                     "apple_access" : {
-                        "expires_at" : 7948503712000
+                        "expires_at" : 7891268083000
                     },
                     "gn5" : {
-                        "expires_at" : 7948503712000
+                        "expires_at" : 7891268083000
                     },
                     "crossplatform_access" : {
-                        "expires_at" : 7948503712000
-                    },
-                    "premium" : {
-                        "expires_at" : 7948503712000
+                        "expires_at" : 7891268083000
                     }
                 },
-                "guid" : "4438a86c-7033-455c-ab39-65837f9aecbf",
-                "identity_provider" : "APPLE",
+                "guid" : "5a1cf8b3-5d48-4418-826b-0530e55f47a4",
+                "identity_provider" : "MICROSOFT",
+                "freemium_max_documents_gnc" : 3
             },
-            "created_at" : "2023-08-25T11:25:02.323644Z",
-            "updated_at" : "2023-08-25T11:25:02.323644Z",
+            "created_at" : "2023-10-09T05:28:31.859693Z",
+            "updated_at" : "2023-10-09T05:28:31.859693Z",
             "schema_url" : "https://kratos.goodnotes.com/schemas/djE",
             "state" : "active"
         },
         "devices" : [
             {
-                "ip_address" : "27.189.6.71",
-                "id" : "f2db1799-39e5-4d8c-ab8f-4ea64d393f91",
-                "user_agent" : "Mozilla/5.0 (iPad; CPU OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1",
+                "ip_address" : "103.212.34.113",
+                "id" : "4aa1fc48-b686-4540-aafd-4088bce89c9b",
+                "user_agent" : "Mozilla/5.0 (iPad; CPU OS 17_0_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0.1 Mobile/15E148 Safari/604.1",
                 "location" : ""
             }
         ],
         "authentication_methods" : [
             {
-                "aal" : "aal1",
                 "method" : "oidc",
-                "completed_at" : "2023-08-29T09:01:35.028862842Z"
+                "aal" : "aal1",
+                "completed_at" : "2023-10-15T03:04:02.010853334Z",
+                "provider" : "microsoft"
             }
         ],
-        "authenticated_at" : "2023-08-29T09:01:35.028865Z"
+        "authenticated_at" : "2023-10-15T03:04:02.010859Z"
     })
 });
