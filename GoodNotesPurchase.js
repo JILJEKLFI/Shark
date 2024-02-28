a5fb7387a2b5bbb2cc0409971b2a016baaedecbd
huozongsheng <huozongsheng@outlook.com>
2024-02-28 23:09:41 +0800

    gn crack

--- GoodNotesPurchase.js
+++ GoodNotesPurchase.js
@@ -5,11 +5,11 @@
 
  [rewrite_local]
 
- ^https:\/\/isi\.csan\.goodnotes\.com(\.cn)?\/nest\/api\/apple\/purchases\/sync url script-echo-response https://raw.githubusercontent.com/JILJEKLFI/Shark/master/GoodNotesPurchase.js
+ ^https:\/\/isi\.csan\.goodnotesapp\.com\.cn\/nest\/api\/apple\/purchases\/sync url script-echo-response https://raw.githubusercontent.com/JILJEKLFI/Shark/master/GoodNotesPurchase.js
 
  [mitm]
 
- hostname=isi.csan.goodnotes.com
+ hostname=isi.csan.goodnotesapp.com.cn
 
  ***********************************/
 
